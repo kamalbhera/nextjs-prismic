4ef86e43657249951752f905d42995389728c348
Kamal Bhera <b438zh@b438zhvyytg91wtp.excitel>
2022-11-10 23:44:19 +0530

    initial commit

---
+++ .slicemachine/assets/slices/HeroSlice/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/HeroSlice';
+
+export default {
+  title: 'slices/HeroSlice'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"cta_link":{"link_type":"Web","url":"https://prismic.io"},"cta_text":"whom"}],"primary":{"title":[{"type":"heading1","text":"Article","spans":[]}],"description":[{"type":"paragraph","text":"Eiusmod reprehenderit ut incididunt sit proident. Laborum voluptate dolor in quis. Sit deserunt dolore et eu magna aute nostrud exercitation consectetur.","spans":[]}],"image":{"dimensions":{"width":900,"height":500},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1596195689404-24d8a8d1c6ea"}},"slice_type":"hero_slice","id":"_Default"}} />
+_Default.storyName = ''
